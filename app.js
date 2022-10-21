292737fd4eca17a88906d6849868ea4e8dde1e07
Yaswanth Kumar Bethu <yash.topaz@gmail.com>
2022-10-22 01:34:28 +0530

    Added tests, husky, add todo, markAsComplete

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+
+const { Todo } = require("./models");
+
+// eslint-disable-next-line no-unused-vars
+app.get("/todos", (request, response) => {
+  console.log("Todo List");
+});
+
+app.post("/todos", async (request, response) => {
+  console.log("Creating a new Todo", request.body);
+  try {
+    const todo = await Todo.addTodo({
+      title: request.body.title,
+      dueDate: request.body.dueDate,
+    });
+    return response.json(todo);
+  } catch (error) {
+    console.log(error);
+    return response.status(422).json(error);
+  }
+});
+
+app.put("/todos/:id/markAsCompleted", async (request, response) => {
+  console.log("We have to update a todo with ID:", request.params.id);
+  const todo = await Todo.findByPk(request.params.id);
+  try {
+    const updatedTodo = await todo.markAsCompleted();
+    return response.json(updatedTodo);
+  } catch (error) {
+    return response.status(422).json(error);
+  }
+});
+
+// eslint-disable-next-line no-unused-vars
+app.delete("/todos/:id", (request, response) => {
+  console.log("Delete a todo by ID:", request.params.id);
+});
+
+module.exports = app;

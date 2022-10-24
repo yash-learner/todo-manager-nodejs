e078bb19323798b5ac49684c5ebbf9cf63afe03f
Yaswanth Kumar Bethu <yash.topaz@gmail.com>
2022-10-24 15:55:28 +0530

    importing external styles to ejs

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const path = require("path");
+
 app.use(bodyParser.json());
 
 const { Todo } = require("./models");
@@ -10,6 +12,10 @@ app.set("view engine", "ejs");
 app.get("/", (request, response) => {
   response.render("index");
 });
+
+// eslint-disable-next-line no-undef
+app.use(express.static(path.join(__dirname, "public")));
+
 // eslint-disable-next-line no-unused-vars
 app.get("/todos", async (request, response) => {
   console.log("Todo List");

65c5def25e6c1c70d038c572e2a26959c8d76a69
Yaswanth Kumar Bethu <yash.topaz@gmail.com>
2022-11-05 00:31:42 +0530

    test

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-undef */
 const app = require("./app");
-
-let server = app.listen(process.env.PORT, () => {
-  console.log(`Started express server at port ${server.address().port}`);
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`server listening at port - ${port}`);
 });

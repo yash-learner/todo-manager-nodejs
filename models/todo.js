273aea4d8d02e54a46b35045d9dd4cf7f3c602d1
Yaswanth Kumar Bethu <yash.topaz@gmail.com>
2022-12-06 20:29:12 +0530

    Added sequelize validation for todo title

--- models/todo.js
+++ models/todo.js
@@ -111,7 +111,14 @@ module.exports = (sequelize, DataTypes) => {
   }
   Todo.init(
     {
-      title: DataTypes.STRING,
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: true,
+          len: 5,
+        },
+      },
       dueDate: DataTypes.DATEONLY,
       completed: DataTypes.BOOLEAN,
     },

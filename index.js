71021dab7e2682be58fcb9165b890fedabb86652
Yaswanth Kumar Bethu <yash.topaz@gmail.com>
2022-12-08 02:10:33 +0530

    Fix app crash

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-undef */
 const app = require("./app");
 
-let server = app.listen(3000 || process.env.PORT, () => {
+let server = app.listen(process.env.PORT, () => {
   console.log(`Started express server at port ${server.address().port}`);
 });
